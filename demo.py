ae8453cae476a55b6e0a73cef9ab59067c34f64d
PANKAJPANDEY7248 <pandeypankaj2003@gmail.com>
2025-11-18 22:32:42 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,19 @@
+# a =9 // 4 # floor division
+# b = 9 % 4 # modules
+# print(a , b)
+# if a == b:
+#     print("right")
+# else :
+#     print("wrong")
+# for i in range (5):
+#     a= i
+#     if  a >= 2:
+#         print("right")
+#     else :
+#         print("wrong")
+# list = [1,3,4]
+mood = input("How you feeling good or bad ").lower()
+if mood =="good":
+    print ("nice")
+else:
+    print("sorry to hear that")
